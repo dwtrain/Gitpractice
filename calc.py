5b028ee4563f62152f772871d1b564752c5e1134
dawit <bekagneri10@gmail.com>
2019-06-30 20:34:40 -0400

    added calc.py and modified test.txt files

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def mult(x,y):
+    return x*y
+
+def divide(x,y):
+    if(y==0):
+        return NULL
+    else:
+        return x/y
+def mod(x,n):
+    return x%n
